3af18d8e9e755a9f7c8299f1a153ac4658477c5f
Assunção Jr <assuncaojr@gmail.com>
2021-02-06 18:05:42 -0300

    feat(swagger): setup API documentation with swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle(app.get('ConfigService').get('projectName'))
+    .setDescription(app.get('ConfigService').get('projectDescription'))
+    .setVersion(process.env.npm_package_version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('/api-doc', app, document);
+
   await app.listen(app.get('ConfigService').get('port'));
 }
 
