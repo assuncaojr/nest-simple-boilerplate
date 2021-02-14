aed58d786fd0519db3b735136f48aef3ba95fc25
Assunção Jr <assuncaojr@gmail.com>
2021-02-14 13:26:55 -0300

    feat(user-entity): setup userEntity

---
+++ src/modules/user/user.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  email: string;
+
+  @Column({
+    unique: true,
+  })
+  password: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
---
+++ src/modules/user/user.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { User } from './user.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UserService],
+  controllers: [UserController],
+  exports: [TypeOrmModule],
+})
+export class UserModule {}
