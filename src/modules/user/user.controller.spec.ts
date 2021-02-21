6b0dbae6aa8d0e37c043caf19f03d043c65eda96
Assunção Jr <assuncaojr@gmail.com>
2021-02-21 20:28:48 -0300

    test(users): added some tests cases to user controller spec

--- src/modules/user/user.controller.spec.ts
+++ src/modules/user/user.controller.spec.ts
@@ -1,11 +1,14 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { MockValueTestingRepository } from '../../setupTest';
+import {
+  MockValueTestingRepository as mockedRepo
+} from '../../setupTest';
 import { UserController } from './user.controller';
 import { User } from './user.entity';
 import { UserModule } from './user.module';
 import { UserService } from './user.service';
+import * as supertest from 'supertest';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -18,7 +21,7 @@ describe('UserController', () => {
         UserService,
         {
           provide: getRepositoryToken(User),
-          useValue: MockValueTestingRepository,
+          useValue: mockedRepo,
         }
       ],
       controllers: [UserController],
@@ -30,9 +33,57 @@ describe('UserController', () => {
     await app.init();
   });
 
-  afterEach(() => app.close());
+  afterEach(() => {
+    mockedRepo.find.mockReset();
+    app.close();
+  });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it ('should return a list of users', async () => {
+    const mockedData = [{
+      firstName: 'John',
+      lastName: 'Smith',
+      email: 'john@mail.com',
+      password: '$rat5artara',
+      active: true,
+    }];
+
+    mockedRepo.find.mockReturnValue(mockedData);
+
+    const { body } = await supertest
+      .agent(app.getHttpServer())
+      .get('/users')
+      .set('Accept', 'application/json')
+      .expect(200);
+
+    expect(body).toEqual(mockedData);
+  });
+
+  it ('should create a user', async () => {
+    const body = {
+      firstName: 'John',
+      lastName: 'Smith',
+      email: 'john@mail.com',
+      password: '123456',
+      isActive: false,
+    };
+
+    /**
+     * Use of Save method instead Create 'cause this
+     * return data to validate response
+     * */
+    mockedRepo.save.mockReturnValue(body);
+
+    const { body: data } = await supertest
+      .agent(app.getHttpServer())
+      .post('/users')
+      .set('Accept', 'application/json')
+      .send(body)
+      .expect(201);
+
+    expect(data).toEqual(body);
   });
 });
