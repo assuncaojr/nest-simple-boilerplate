84327d3b5503623a506064d61edb36e402942201
Assunção Jr <assuncaojr@gmail.com>
2021-02-27 07:55:10 -0300

    fix(user): fix userId type

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -14,7 +14,7 @@ export class UserService {
     return this.usersRepository.find();
   }
 
-  findOne(id: string): Promise<User> {
+  findOne(id: number): Promise<User> {
     return this.usersRepository.findOne(id);
   }
 
